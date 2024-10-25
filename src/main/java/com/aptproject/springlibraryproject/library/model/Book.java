4d0180ce3dd01f69860f205d027d547e1bc4e1e1
stud <nikasnow44@gmail.com>
2024-10-25 11:30:39 +0300

    book and genre

--- src/main/java/com/aptproject/springlibraryproject/library/model/Book.java
+++ src/main/java/com/aptproject/springlibraryproject/library/model/Book.java
@@ -1,4 +1,46 @@
 package com.aptproject.springlibraryproject.library.model;
 
-public class Book {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SequenceGenerator(name = "default_generator", sequenceName = "book_sequence", allocationSize = 1)
+public class Book  extends GenericModel{
+
+    @Column(name = "title", nullable = false)
+    private String bookTitle;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "publish_date")
+    private LocalDate publishDate;
+
+    @Column(name = "amount")
+    private Integer amount;
+
+
+    @Column(name = "storage_palace", nullable = false)
+    private String storagePlace;
+
+    @Column(name = "online_copy_path")
+    private String onlineCopyPath;
+
+    @Column(name = "genre", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Genre genre;
+
+    @Column(name = "description")
+    private String description;
 }
