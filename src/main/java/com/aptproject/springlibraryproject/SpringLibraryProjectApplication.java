c8e2e8ea3f3128a6b2db91ff89f5e8256cd2965e
мек <nikasnow44@gmail.com>
2024-10-25 08:21:11 +0300

    don't work get book :(

--- src/main/java/com/aptproject/springlibraryproject/SpringLibraryProjectApplication.java
+++ src/main/java/com/aptproject/springlibraryproject/SpringLibraryProjectApplication.java
@@ -1,22 +1,34 @@
 package com.aptproject.springlibraryproject;
 
-import com.aptproject.springlibraryproject.dbexample.dao.BookDaoJDBC;
+import com.aptproject.springlibraryproject.dbexample.dao.BookDAOBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @SpringBootApplication
 public class SpringLibraryProjectApplication implements CommandLineRunner {
+
+    private BookDAOBean bookDAOBean;
+
+    public SpringLibraryProjectApplication(BookDAOBean bookDAOBean) {
+        this.bookDAOBean = bookDAOBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringLibraryProjectApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception{
-        BookDaoJDBC bookdaoJDBC = new BookDaoJDBC();
-        bookdaoJDBC.findBookById(3);
-    }
+//    @Autowired
+//    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    @Override
+    public void run(String... args) throws Exception {
+        bookDAOBean.findBookById(4);
+
+
+    }
 }
+
 
---
+++ src/main/java/com/aptproject/springlibraryproject/dbexample/MyDBConfigContext.java
@@ -0,0 +1,21 @@
+package com.aptproject.springlibraryproject.dbexample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.aptproject.springlibraryproject.dbexample.constants.DBConstants.*;
+
+@Configuration
+public class MyDBConfigContext {
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://" + DB_HOST + ":" + PORT + "/" +
+                DB, USER, PASSWORD);
+    }
+
+}
