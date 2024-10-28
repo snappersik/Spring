c8a53abbd544f13bf8fecae94b3afe7bcc7cc9e3
stud <nikasnow44@gmail.com>
2024-10-28 11:14:19 +0300

    add User in library

---
+++ src/main/java/com/aptproject/springlibraryproject/library/model/User.java
@@ -0,0 +1,45 @@
+package com.aptproject.springlibraryproject.library.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity 
+@Table(name = "users",
+        uniqueConstraints = {@UniqueConstraint(name = "uniqueEmail", columnNames = "email"),
+                @UniqueConstraint(name = "uniqueLogin", columnNames = "login")})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SequenceGenerator(name = "default_generator", sequenceName = "users_seq", allocationSize = 1)
+public class User extends GenericModel{
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+}
